f7073f6444b2fa5b42841e8b22609948af4c7717
Matt Williams <matthew.williams@iovation.com>
2020-01-16 12:19:49 -0800

    Updated html, app.py needs review

--- python/todoapp/app.py
+++ python/todoapp/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://\
@@ -21,11 +22,13 @@ db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.form.get('description', '')
+    description = request.get_json()['description']
     todo = Todo(description=description)
     db.session.add(todo)
     db.session.commit()
-    return redirect(url_for('index'))
+    return jsonify({
+      'description': todo.description
+    })
 
 
 @app.route('/')
